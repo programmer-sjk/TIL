cfa5b3ccd9879dec83b0018939963cf70ecac3e4
programmer-sjk <wjdrnr6662@gmail.com>
2026-02-28 12:39:17 +0900

    feat: user crud rest api 추가

--- language/go/simple-server/main.go
+++ language/go/simple-server/main.go
@@ -9,5 +9,29 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
+	// CRUD API
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.SendString("유저 목록 조회")
+	})
+
+	app.Get("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("유저 조회: " + id)
+	})
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		return c.SendString("유저 생성")
+	})
+
+	app.Put("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("유저 수정: " + id)
+	})
+
+	app.Delete("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		return c.SendString("유저 삭제: " + id)
+	})
+
 	app.Listen(":3000")
 }
